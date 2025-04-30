04b143cd590f97036aa25d22e9aaabd56a5268c2
YassineHachguer <yassine.hachguer05@gmail.com>
2025-04-30 18:07:32 +0100

    implementation de la couche web pour la gestion des comptes et des opérations

--- ebank-backend/src/main/java/com/example/ebankbackend/web/BankAccountRestController.java
+++ ebank-backend/src/main/java/com/example/ebankbackend/web/BankAccountRestController.java
@@ -1,4 +1,42 @@
 package com.example.ebankbackend.web;
 
+import com.example.ebankbackend.dtos.AccountHistoryDTO;
+import com.example.ebankbackend.dtos.BankAccountDTO;
+import com.example.ebankbackend.dtos.OperationDTO;
+import com.example.ebankbackend.exeptions.BankAccountNotFoundException;
+import com.example.ebankbackend.services.BankAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin("*")
 public class BankAccountRestController {
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/accounts/{accountId}")
+    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        return  bankAccountService.getBankAccount(accountId);
+    }
+    @GetMapping("/accounts")
+    public List<BankAccountDTO> listAccounts(){
+        return bankAccountService.bankAccountList();
+    }
+    @GetMapping("/accounts/{accountId}/operations")
+    public List<OperationDTO> getHistory(@PathVariable String accountId){
+        return bankAccountService.accountHistory(accountId);
+    }
+
+    @GetMapping("/accounts/{accountId}/pageOperations")
+    public AccountHistoryDTO getAccountHistory(
+            @PathVariable String accountId,
+            @RequestParam(name="page",defaultValue = "0") int page,
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
+        return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+
+
+
 }
