000fe4fae1018ed31575a0b38a4afb4b312a94c9
YassineHachguer <yassine.hachguer05@gmail.com>
2025-05-02 16:13:25 +0100

    implémentation de la page des customers

--- ebank-frontend/src/app/app.component.ts
+++ ebank-frontend/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,
+    HttpClientModule,
+    ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
