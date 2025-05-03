8433bf147b790d99f365d71842a9f9037093d238
YassineHachguer <yassine.hachguer05@gmail.com>
2025-05-03 22:25:01 +0100

    fin de la partie frontend

--- ebank-frontend/src/app/app.config.ts
+++ ebank-frontend/src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // ✅ Import ici
+import { appHttpInterceptor } from './interceptors/app-http.interceptor';
+import {ApplicationConfig} from "@angular/core";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideRouter} from "@angular/router";
+import {routes} from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient() // ✅ Ajoute ceci pour activer HttpClient
+    provideHttpClient(
+      withInterceptors([appHttpInterceptor])
+    ),
+    provideRouter(routes)
   ]
 };
--- ebank-frontend/src/app/app.routes.ts
+++ ebank-frontend/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
 import {NewCustomerComponent} from "./new-customer/new-customer.component";
+import {LoginComponent} from "./login/login.component";
+import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts.component";
+import {AdminTemplateComponent} from "./admin-template/admin-template.component";
 
 export const routes: Routes = [
-  {path:"customers", component: CustomersComponent},
-  {path:"accounts", component: AccountsComponent},
-  { path :"new-customer", component : NewCustomerComponent},
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo: "/login", pathMatch:"full"},
+  {path:"admin", component:AdminTemplateComponent, children:[
+      {path:"customers", component: CustomersComponent},
+      {path:"accounts", component: AccountsComponent},
+      { path :"new-customer", component : NewCustomerComponent},
+      { path :"customer-accounts/:id", component : CustomerAccountsComponent},
+    ]},
+
+
 ];
